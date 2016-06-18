e53ade86744083dae30eb9d3547c87dade361b87
Glenn Wiley <glennwiley@gmail.com>
2016-06-18 13:34:33 -0500

    added m1 project and shell program

---
+++ src/m1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+
+namespace m1
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var cfg = new Configuration();
+			cfg.DataBaseIntegration(x =>
+			{
+				x.ConnectionString = "Server=SMACCO\\SQLEXPRESS;Database=NHibernateDemo;Integrated Security=SSPI;";
+				x.Driver<SqlClientDriver>();
+				x.Dialect<MsSql2012Dialect>();
+			});
+			cfg.AddAssembly(Assembly.GetExecutingAssembly());
+			var sessionFactory = cfg.BuildSessionFactory();
+			using (var session = sessionFactory.OpenSession())
+			using(var tx = session.BeginTransaction())
+			{
+				tx.Commit();
+			}
+		}
+	}
+}
